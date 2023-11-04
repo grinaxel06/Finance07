f3619dbd7b551f4ed14740f92a5565e42aea4d11
GRISHA AKSENOV <grin_axel@inbox.ru>
2023-11-04 20:55:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/finance/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.finance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.method.CharacterPickerDialog;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialButton show;
+    MaterialButton show1;
+    Dialog dialog;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        show = findViewById(R.id.plus_button);
+        show1 = findViewById(R.id.minus_button);
+        dialog = new Dialog(MainActivity.this);
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+        show1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
+    }
+
+    private void showCustomDialog() {
+        dialog.setContentView(R.layout.regular_or_single);
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        MaterialButton btt_on_AddOperation = dialog.findViewById(R.id.single_btt);
+
+        View.OnClickListener oclBtt_on_AddOperation = new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, AddOperation.class);
+                startActivity(intent);
+            }
+        };
+
+        btt_on_AddOperation.setOnClickListener(oclBtt_on_AddOperation);
+    }
+
+
+}
